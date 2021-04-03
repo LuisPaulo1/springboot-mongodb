2dabc3e436f2b7da072f6f3086078f04844b4996
LuisPaulo1 <csluispaulo@gmail.com>
2021-04-03 20:06:48 -0300

    Obtendo um usuário por id

---
+++ src/main/java/com/meudominio/springbootmongodb/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,10 @@
+package com.meudominio.springbootmongodb.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+	
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+}
