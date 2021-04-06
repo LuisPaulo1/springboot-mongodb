926b6130fde36f75ff9d887fd524adf4cf1be9fb
LuisPaulo1 <csluispaulo@gmail.com>
2021-04-05 21:21:06 -0300

    Obtendo um post por id

---
+++ src/main/java/com/meudominio/springbootmongodb/services/PostService.java
@@ -0,0 +1,22 @@
+package com.meudominio.springbootmongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.meudominio.springbootmongodb.domain.Post;
+import com.meudominio.springbootmongodb.repository.PostRepository;
+import com.meudominio.springbootmongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
